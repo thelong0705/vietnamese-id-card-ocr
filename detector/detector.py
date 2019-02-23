9b0470545dd88fe77e5b31fbf016f3d0c9cbba0f
TheLong <thelong0705@gmail.com>
2019-02-23 09:04:05 +0700

    try another way to detect

--- detector/detector.py
+++ detector/detector.py
@@ -2,50 +2,72 @@ import numpy as np
 import cv2
 import imutils
 from transform import four_point_transform
-
 # image_path = 'test_images/image5.jpg'
 image_path = 'test_images/8.png'
 img = cv2.imread(image_path)
-left, right, top, bottom = 364, 1371, 259, 886
+# left, right, top, bottom = 364, 1371, 259, 886
 # img = img[top:bottom, left:right]
 height, width, channels = img.shape
 if height > 500:
     img = imutils.resize(img, height=500)
 
-orig = img.copy
+orig = img.copy()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
+# gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
-
-
 cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
 cnts = sorted(cnts, key=lambda c: cv2.arcLength(c, False), reverse=True)[:5]
 c = cnts[0]
+
 peri = cv2.arcLength(c, True)
 approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 screenCnt = approx
 warped = four_point_transform(orig, screenCnt.reshape(4, 2))
-height, width, channels = warped.shape
+cv2.imwrite("test2.png",warped)
 
 
 
-number_img = warped[int(0.25*height):int(0.37*height), int(0.46*width):int(0.86*width)]
-name_img = warped[int(0.4*height):int(0.5*height), int(0.46*width):int(width)]
-dob = warped[int(0.52*height):int(0.6*height), int(0.61*width):int(0.77*width)]
-sex = warped[int(0.58*height):int(0.68*height), int(0.45*width):int(0.55*width)]
-nationality = warped[int(0.58*height):int(0.68*height), int(0.72*width):int(width)]
-country = warped[int(0.7*height):int(0.8*height), int(0.55*width):int(width)]
-address1 = warped[int(0.8*height):int(0.87*height), int(0.55*width):int(width)]
-address2 = warped[int(0.87*height):int(0.95*height), int(0.4*width):int(width)]
 
 
-cv2.imwrite('number.png', number_img)
-cv2.imwrite('name.png', name_img)
-cv2.imwrite('dob.png', dob)
-cv2.imwrite('sex.png', sex)
-cv2.imwrite('nationality.png', nationality)
-cv2.imwrite('country.png', country)
-cv2.imwrite('address1.png', address1)
-cv2.imwrite('address2.png', address2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# number_img = warped[int(0.25*height):int(0.37*height), int(0.46*width):int(0.86*width)]
+# name_img = warped[int(0.4*height):int(0.5*height), int(0.46*width):int(width)]
+# dob = warped[int(0.52*height):int(0.6*height), int(0.61*width):int(0.77*width)]
+# sex = warped[int(0.58*height):int(0.68*height), int(0.45*width):int(0.55*width)]
+# nationality = warped[int(0.58*height):int(0.68*height), int(0.72*width):int(width)]
+# country = warped[int(0.7*height):int(0.8*height), int(0.55*width):int(width)]
+# address1 = warped[int(0.8*height):int(0.87*height), int(0.55*width):int(width)]
+# address2 = warped[int(0.87*height):int(0.95*height), int(0.4*width):int(width)]
+
+
+# cv2.imwrite('number.png', number_img)
+# cv2.imwrite('name.png', name_img)
+# cv2.imwrite('dob.png', dob)
+# cv2.imwrite('sex.png', sex)
+# cv2.imwrite('nationality.png', nationality)
+# cv2.imwrite('country.png', country)
+# cv2.imwrite('address1.png', address1)
+# cv2.imwrite('address2.png', address2)
 
--- detector/mser.py
+++ detector/mser.py
@@ -2,43 +2,33 @@ import cv2
 import numpy as np
 import imutils
 #Create MSER object
-mser = cv2.MSER_create()
+mser = cv2.MSER_create(_delta=4, _max_area=1000)
 
 #Your image path i-e receipt path
-image_path = 'test_images/image4.jpg'
+image_path = 'cropped.png'
 
 img = cv2.imread(image_path)
 # left, right, top, bottom = 364, 1371, 259, 886
-left, right, top, bottom = 414, 1233, 300, 879
+# left, right, top, bottom = 414, 1233, 300, 879
 
-img = img[top:bottom, left:right]
-height, width, channels = img.shape
+# img = img[top:bottom, left:right]
 
 #Convert to gray scale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 vis = img.copy()
 
-#detect regions in gray scale image
-regions, _ = mser.detectRegions(gray)
 
+regions, bboxs = mser.detectRegions(gray)
 hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
-
-cv2.polylines(vis, hulls, 1, (0, 255, 0))
-
-cv2.imshow('img', vis)
-
-cv2.waitKey(0)
-
 mask = np.zeros((img.shape[0], img.shape[1], 1), dtype=np.uint8)
-
+index = 0 
 for contour in hulls:
+    index += 1
+    cv2.drawContours(mask, [contour], 0, (255, 255, 255), -1)
 
-    cv2.drawContours(mask, [contour], -1, (255, 255, 255), -1)
+im2, contours, hierarchy = cv2.findContours(
+    mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-#this is used to find only text regions, remaining are ignored
-text_only = cv2.bitwise_and(img, img, mask=mask)
+cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 
-cv2.imshow("text only", text_only)
-
-cv2.waitKey(0)
