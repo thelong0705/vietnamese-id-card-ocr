7164c5cfabe1d37c72a9348325b2ad8bb4fcb243
TheLong <thelong0705@gmail.com>
2019-03-06 18:14:13 +0700

    get each line of id

--- detector/detector.py
+++ detector/detector.py
@@ -1,12 +1,14 @@
 import numpy as np
 import cv2
 import imutils
+import argparse
 
 
 def get_important_part(img):
     h, w, _ = img.shape
     img = img[int(0.25*h):h, int(0.35*w):w]
     return img
+
 
 def show_img(img):
     cv2.imshow('', img)
@@ -18,30 +20,52 @@ def draw_rec(list_rec_tuple, img):
         x, y, w, h = rec_tuple
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-image_path = 'test/not-normal.png'
+
+def grouper(iterable, index, threshold):
+    prev = ()
+    group = []
+    for item in iterable:
+        if not prev or item[index] - prev[index] <= threshold:
+            group.append(item)
+        else:
+            yield group
+            group = [item]
+        prev = item
+    if group:
+        yield group
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+                help="Path to the image to be scanned")
+args = vars(ap.parse_args())
+image_path = args['image']
 img = cv2.imread(image_path)
+img = get_important_part(img)
 h, w, _ = img.shape
 if h > 500:
+    ratio = h/500
     img = imutils.resize(img, height=500)
-img = get_important_part(img)
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(img_gray, (5, 5), 0)
-edged = cv2.Canny(gray, 75, 200)
-_, contours, _ = cv2.findContours(
-    edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in contours]
-mask = np.zeros((img.shape[0], img.shape[1], 1), dtype=np.uint8)
-for contour in hulls:
-    cv2.drawContours(mask, [contour], 0, (255, 255, 255), -1)
-
-_, contours, _ = cv2.findContours(
-    mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+kernel = np.ones((25, 25), np.uint8)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, kernel)
+thresh = cv2.threshold(blackhat, 0, 255, cv2.THRESH_OTSU)[1]
+kernel = np.ones((1, w), np.uint8)
+closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+# show_img(closing)
+_, cnts, _ = cv2.findContours(closing.copy(), cv2.RETR_EXTERNAL,
+                              cv2.CHAIN_APPROX_SIMPLE)
 locs = []
-for cnt in contours:
-    x, y, w, h = cv2.boundingRect(cnt)
+for contour in cnts:
+    x, y, w, h = cv2.boundingRect(contour)
     locs.append((x, y, w, h))
 
-draw_rec(locs, img)
-show_img(img)
+locs.sort(key=lambda tup: tup[1])
+id_group = locs[0]
+name_group = locs[1]
+dob_group = locs[2]
+gender_and_nation_group = locs[3]
+country_group = locs[4]
+address_first = locs[5]
+address_second = locs[6]
