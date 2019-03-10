50dc9525def44ff9028fb68952af8998b2fe5866
TheLong <thelong0705@gmail.com>
2019-03-10 13:33:57 +0700

    add ocr to main

--- detector/detector.py
+++ detector/detector.py
@@ -10,9 +10,15 @@ def show_img(img):
     cv2.waitKey(0)
 
 
+def draw_rec(list_rec_tuple, img):
+    for rec_tuple in list_rec_tuple:
+        x, y, w, h = rec_tuple
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+
 def get_max_box(group):
     xmin = min(group, key=lambda tup: tup[0])[0]
-    xmax, _, w, _ = max(group, key=lambda tup: tup[0]+tup[3])
+    xmax, _, w, _ = max(group, key=lambda tup: tup[0]+tup[2])
     xmax = xmax + w
     return xmin, xmax
 
@@ -40,7 +46,6 @@ def process_id_part(img):
         blackhat, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     kernel = np.ones((2, 2), np.uint8)
     opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-
     _, cnts, _ = cv2.findContours(
         opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     locs = []
@@ -52,6 +57,38 @@ def process_id_part(img):
     xmin, xmax = get_max_box(number_locs)
     id_img = img[0: img.shape[0], xmin-5:xmax]
     return id_img
+
+
+# def process_name(img):
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#     lower = np.array([0, 0, 0], np.uint8)
+#     upper = np.array([180, 255, 127], np.uint8)
+#     mask = cv2.inRange(img, lower, upper)
+#     kernel = np.ones((5, 2), np.uint8)
+#     closing = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+#     _, contours, _ = cv2.findContours(
+#         closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#     show_img(closing)
+#     locs = []
+#     for cnt in contours:
+#         x, y, w, h = cv2.boundingRect(cnt)
+#         locs.append([x, y, w, h])
+#     locs.sort(key=lambda tup: tup[0])
+#     xmin, xmax = get_max_box(locs)
+#     img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+#     name_img = img[0: img.shape[0], xmin-5:img.shape[1]]
+#     return name_img
+
+
+def get_part(locs, img, ratio):
+    x, y, w, h = tuple(int(ratio * l) for l in locs)
+    img = img[y-5: y+h+5, x:x+w]
+    return img
+
+
+def process_name(img):
+    h, w, _ = img.shape
+    return img[0:h, int(0.3*w):w]
 
 
 def get_information(img):
@@ -84,7 +121,13 @@ def get_information(img):
     country_group = locs[4]
     address_first = locs[5]
     address_second = locs[6]
-    x, y, w, h = tuple(int(ratio * element) for element in id_group)
-    id_img = orig[y-5: y+h+5, x:x+w]
+    id_img = get_part(id_group, orig, ratio)
     id_img = process_id_part(id_img)
-    return id_img
+    name_img = get_part(name_group, orig, ratio)
+    name_img = process_name(name_img)
+    dob_img = get_part(dob_group, orig, ratio)
+    gender_and_nation_img = get_part(gender_and_nation_group, orig, ratio)
+    country_img = get_part(country_group, orig, ratio)
+    address_first_img = get_part(address_first, orig, ratio)
+    address_second_img = get_part(address_second, orig, ratio)
+    return id_img, name_img, dob_img, gender_and_nation_img, country_img, address_first_img, address_second_img
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import cv2
 import sys
 from cropper.cropper import crop_card
 from detector.detector import get_information
+from PIL import Image
+import pytesseract
+import os
 
 
 def show_img(img):
@@ -15,6 +18,20 @@ ap.add_argument("-i", "--image", required=True,
                 help="Path to the image to be scanned")
 args = vars(ap.parse_args())
 warped = crop_card(args['image'])
-id_img = get_information(warped)
-show_img(id_img)
-cv2.imwrite("id.png", id_img)
+group_tuple = get_information(warped)
+
+index = 0
+for t in group_tuple:
+    language = 'vie'
+    if index == 0 or index == 2:
+        language = 'eng'
+    if index != 0:
+        t = cv2.cvtColor(t, cv2.COLOR_BGR2GRAY)
+        _, th = cv2.threshold(t, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    filename = "xam.png"
+    cv2.imwrite(filename, t)
+    text = pytesseract.image_to_string(Image.open(
+        filename), lang=language, config='--psm 7')
+    os.remove(filename)
+    print(text)
+    index = index + 1
