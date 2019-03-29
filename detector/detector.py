faef7667d9e4062ed08b684c3e409f830f0cc663
TheLong <thelong0705@gmail.com>
2019-03-29 20:40:33 +0700

    rewrite detector.py

--- detector/detector.py
+++ detector/detector.py
@@ -1,6 +1,6 @@
-import numpy as np
 import cv2
 import imutils
+import numpy as np
 import statistics
 import copy
 
@@ -10,10 +10,10 @@ def show_img(img):
     cv2.waitKey(0)
 
 
-def draw_rec(list_rec_tuple, img, ratio=1):
-    for rec_tuple in list_rec_tuple:
-        x, y, w, h = tuple(int(ratio * l) for l in rec_tuple)
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+def cropout_unimportant_part(img):
+    h, w, _ = img.shape
+    img = img[int(0.25*h):h, int(0.35*w):w]
+    return img
 
 
 def resize_img(img):
@@ -24,287 +24,202 @@ def resize_img(img):
     return (img, ratio)
 
 
-def find_color_threshold(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
-    thresh = cv2.bitwise_not(thresh)
-    _, contours, _ = cv2.findContours(
-        thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    locs = []
-    mask = np.zeros(img.shape[:2], np.uint8)
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    cv2.drawContours(mask, contours, -1, 255, -1)
-    return cv2.mean(img_hsv, mask)[2]
-
-
-def get_max_box(group):
-    xmin, ymin, _, _ = min(group, key=lambda tup: tup[0])
-    xmax, _, w, _ = max(group, key=lambda tup: tup[0]+tup[2])
-    xmax = xmax + w
-    return xmin, ymin, xmax
-
-
-def cropout_unimportant_part(img):
-    h, w, _ = img.shape
-    img = img[int(0.25*h):h, int(0.35*w):w]
-    return img
-
-
-def remove_shorter_than_med(group, med):
-    group_orig = copy.deepcopy(group)
-    for element in group_orig:
-        if element[-1] <= 0.75*med:
-            group.remove(element)
-    return group
-
-
-# def remove_smaller_area(group, avg):
-#     group_orig = copy.deepcopy(group)
-#     for element in group_orig:
-#         if element[-1] * element[-2] < avg:
-#             group.remove(element)
-#     return group
-
-
-def process_id_part(img):
-    show_img(img)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    kernel = np.ones((25, 20), np.uint8)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, kernel)
-    thresh = cv2.threshold(
-        blackhat, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    kernel = np.ones((2, 2), np.uint8)
-    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-    _, cnts, _ = cv2.findContours(
-        opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    locs = []
-    for cnt in cnts:
-        x, y, w, h = cv2.boundingRect(cnt)
-        locs.append((x, y, w, h))
-    med = statistics.median(map(lambda t: t[-1], locs))
-    number_locs = remove_shorter_than_med(locs, med)
-    xmin, xmax = get_max_box(number_locs)
-    id_img = img[0: img.shape[0], xmin-5:xmax]
-    return id_img
-
-
-def get_part(locs, img, ratio):
-    x, y, w, h = tuple(int(ratio * l) for l in locs)
-    img = img[y: y+h, x:x+w]
-    return img
-
-
-def split_half_by_distance(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
-    thresh = cv2.bitwise_not(thresh)
-    # show_img(thresh)
-    _, contours, _ = cv2.findContours(
-        thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    locs = []
-    for cnt in contours:
-        x, y, w, h = cv2.boundingRect(cnt)
-        locs.append([x, y, w, h])
-    locs.sort(key=lambda tup: tup[0])
-    max_distance = 0
-    index = 0
-    for idx, current_location in enumerate(locs):
-        if idx >= len(locs) - 1:
-            break
-        next_location = locs[idx+1]
-        end_box_loc = current_location[0] + current_location[2]
-        diff = next_location[0] - end_box_loc
-        if diff > max_distance:
-            max_distance = diff
-            index = idx
-    first_half = locs[0:index + 1]
-    second_half = locs[index + 1:]
-    return first_half, second_half
-
-
-def process_name(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    kernel = np.ones((25, 25), np.uint8)
-    blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, kernel)
-    thresh = cv2.threshold(
-        blackhat, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    h, w = thresh.shape
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (w, 5))
-    dilation = cv2.dilate(thresh, kernel, iterations=2)
-    _, cnts, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
-                                  cv2.CHAIN_APPROX_SIMPLE)
-    locs = []
-    for contour in cnts:
-        x, y, w, h = cv2.boundingRect(contour)
-        locs.append((x, y, w, h))
-    max_locs = max(locs, key=lambda tup: tup[2] * tup[3])
-    x, y, w, h = max_locs
-    return img[y:y+h, x:x+w]
-
-
-def process_gender(img):
-    locs = split_half_by_distance(img)[0]
-    xmin, xmax = get_max_box(locs)
-    gender_img = img[0: img.shape[0], xmin:xmax+5]
-    return gender_img
-
-
-def process_country(img):
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    color_threshhold = find_color_threshold(img)
-    lower = np.array([0, 0, 0], np.uint8)
-    upper = np.array([180, 255, int(color_threshhold-20)], np.uint8)
-    mask = cv2.inRange(img_hsv, lower, upper)
-    _, contours, _ = cv2.findContours(
-        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    show_img(mask)
-    locs = []
-    for cnt in contours:
-        x, y, w, h = cv2.boundingRect(cnt)
-        locs.append([x, y, w, h])
-    avg = statistics.mean(map(lambda t: t[-1] * t[-2], locs))
-    locs = remove_smaller_area(locs, avg)
-    # draw_rec(locs,img)
-    show_img(img)
-    xmin, xmax = get_max_box(locs)
-    xmin = xmin - 5 if xmin - 5 > 0 else xmin
-    country_img = img[0: img.shape[0], xmin:xmax+30]
-    return country_img
-
-
-def process_first_address(img):
-    return process_country(img)
-
-
-def get_information(img):
-    img = cropout_unimportant_part(img)
-    orig = img.copy()
-    h, w, _ = img.shape
-    img, ratio = resize_img(img)
-    kernel = np.ones((25, 25), np.uint8)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, kernel)
-    thresh = cv2.threshold(
-        blackhat, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    kernel = np.ones((5, w), np.uint8)
-    closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-    _, cnts, _ = cv2.findContours(closing.copy(), cv2.RETR_EXTERNAL,
-                                  cv2.CHAIN_APPROX_SIMPLE)
-    locs = []
-    for contour in cnts:
-        x, y, w, h = cv2.boundingRect(contour)
-        locs.append((x, y, w, h))
-    locs.sort(key=lambda tup: tup[2] * tup[3], reverse=True)
-    locs = locs[:7]
-    draw_rec(locs, orig, ratio)
-    return orig
-    locs.sort(key=lambda tup: tup[1])
-    id_group = locs[0]
-    name_group = locs[1]
-    dob_group = locs[2]
-    gender_and_nation_group = locs[3]
-    country_group = locs[4]
-    address_first = locs[5]
-    address_second = locs[6]
-    id_img = get_part(id_group, orig, ratio)
-    id_img = process_id_part(id_img)
-    name_img = get_part(name_group, orig, ratio)
-    # name_img = process_name(name_img)
-    dob_img = get_part(dob_group, orig, ratio)
-    gender_and_nation_img = get_part(gender_and_nation_group, orig, ratio)
-    gender_img = process_gender(gender_and_nation_img)
-    country_img = get_part(country_group, orig, ratio)
-    # country_img = process_country(country_img)
-    address_first_img = get_part(address_first, orig, ratio)
-    # address_first_img = process_first_address(address_first_img)
-    address_second_img = get_part(address_second, orig, ratio)
-    return id_img, name_img, dob_img, gender_img, country_img, address_first_img, address_second_img
-
-
-def detector(img):
-    img = cropout_unimportant_part(img)
-    img, ratio = resize_img(img)
-    label_img = crop_label(img)
-    h, w, _ = label_img.shape
-    gray = cv2.cvtColor(label_img, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.threshold(
-        gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    kernel = np.ones((5, w), np.uint8)
-    closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-    _, cnts, _ = cv2.findContours(closing.copy(), cv2.RETR_EXTERNAL,
-                                  cv2.CHAIN_APPROX_SIMPLE)
-    locs = []
-    for contour in cnts:
-        x, y, w, h = cv2.boundingRect(contour)
-        locs.append((x, y, w, h))
-    locs.sort(key=lambda tup: tup[2] * tup[3], reverse=True)
-    locs = locs[:5]
-    info_parts = get_info_parts(locs, img)
-    name_part = info_parts[0]
-    name = process_name(name_part)
-    name = cut_name(name)
-
-
 def crop_label(img):
     h, w, _ = img.shape
     img = img[0:h, 0:int(0.05 * w)]
     return img
 
 
-def get_info_parts(list_rec_tuple, img):
-    list_rec_tuple.sort(key=lambda tup: tup[1])
-    height, width, _ = img.shape
-    list_info = []
-    for index, l in enumerate(list_rec_tuple):
-        x, y, w, h = l
-        y = y - 20
-        if index != len(list_rec_tuple) - 1:
-            x1, y1, _, _ = list_rec_tuple[index+1]
-            list_info.append(img[y:y1, x:width])
-        else:
-            list_info.append(img[y:height, x:width])
-    return list_info
-
-
-def cut_name(img):
+def get_threshold_img(img, kernel):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    kernel = np.ones((25, 25), np.uint8)
     blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, kernel)
     thresh = cv2.threshold(
         blackhat, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    _, cnts, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL,
+    return thresh
+
+
+def get_contour_boxes(img):
+    _, cnts, _ = cv2.findContours(img, cv2.RETR_EXTERNAL,
                                   cv2.CHAIN_APPROX_SIMPLE)
-    locs = []
-    for contour in cnts:
-        x, y, w, h = cv2.boundingRect(contour)
-        locs.append((x, y, w, h))
-    locs = remove_name_label(locs)
-    # draw_rec(locs,img)
-    # show_img(img)
-    locs = remove_smaller_area(locs)
-    xmin, ymin, xmax = get_max_box(locs)
-    name_img = img[ymin-10: img.shape[0], xmin:img.shape[1]]
-    show_img(name_img)
-    return name_img
+    contour_boxes = []
+    for cnt in cnts:
+        contour_boxes.append(cv2.boundingRect(cnt))
+    return contour_boxes
 
 
-def remove_smaller_area(group):
-    avg = statistics.median(map(lambda t: t[-1] * t[-2], group))
-    group_orig = copy.deepcopy(group)
-    for element in group_orig:
-        if element[-1] * element[-2] < 0.5 * avg:
-            group.remove(element)
-    return group
+def get_info_list(img, contour_boxes):
+    contour_boxes.sort(key=lambda tup: tup[1])
+    height, width, _ = img.shape
+    list_info = []
+    for index, l in enumerate(contour_boxes):
+        x, y, w, h = l
+        y = y - 20
+        if index != len(contour_boxes) - 1:
+            x1, y1, _, _ = contour_boxes[index+1]
+            list_info.append((x, y, width, y1))
+        else:
+            list_info.append((x, y, width, height))
+    return list_info
 
 
-def remove_name_label(group):
+def get_main_text(img, box, kernel_height):
+    x0, y0, x1, y1 = box
+    img = img[y0:y1, x0:x1]
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
+    thresh = get_threshold_img(img, kernel)
+    kernel = cv2.getStructuringElement(
+        cv2.MORPH_RECT, (thresh.shape[1], kernel_height))
+    dilation = cv2.dilate(thresh, kernel, iterations=1)
+    contour_boxes = get_contour_boxes(dilation)
+    max_box = max(contour_boxes, key=lambda tup: tup[2] * tup[3])
+    x, y, w, h = max_box
+    return (x0+x, y0+y, x0+x+w, y0+y+h)
+
+
+def remove_name_label(group, width):
     avg = statistics.mean(map(lambda t: t[1], group))
     group_orig = copy.deepcopy(group)
     for element in group_orig:
-        if element[1] < avg:
+        if element[1] < avg and element[0] < width/4:
             group.remove(element)
     return group
 
 
+def remove_smaller_area(group, width):
+    avg = statistics.median(map(lambda t: t[-1] * t[-2], group))
+    group_orig = copy.deepcopy(group)
+    for element in group_orig:
+        if element[-1] * element[-2] < 0.5 * avg and element[0] < width/4:
+            group.remove(element)
+    return group
+
+
+def find_max_box(group):
+    xmin = min(group, key=lambda t: t[0])[0]
+    ymin = min(group, key=lambda t: t[1])[1]
+    xmax_box = max(group, key=lambda t: t[0] + t[2])
+    xmax = xmax_box[0] + xmax_box[2]
+    ymax_box = max(group, key=lambda t: t[1] + t[3])
+    ymax = ymax_box[1] + ymax_box[3]
+    return (xmin, ymin, xmax - xmin, ymax - ymin)
+
+
+def get_name(img, box):
+    x0, y0, x1, y1 = box
+    img = img[y0:y1, x0:x1]
+    height, width, _ = img.shape
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
+    thresh_img = get_threshold_img(img, kernel)
+    contour_boxes = get_contour_boxes(thresh_img)
+    contour_boxes = remove_name_label(contour_boxes, width)
+    contour_boxes = remove_smaller_area(contour_boxes, width)
+    x, y, w, h = find_max_box(contour_boxes)
+    return (x0+x, y0+y, x0+x+w, y0+y+h)
+
+
+def get_img_from_box(orig, ratio, box):
+    x0, y0, x1, y1 = tuple(int(ratio * element) for element in box)
+    return orig[y0:y1, x0:x1]
+
+
+def get_text_from_two_lines(img, box):
+    x0, y0, x1, y1 = box
+    img = img[y0:y1, x0:x1]
+    kernel = np.ones((25, 25), np.uint8)
+    thresh = get_threshold_img(img, kernel)
+    height, width = thresh.shape
+    contour_boxes = get_contour_boxes(thresh)
+    boxes_copy = copy.deepcopy(contour_boxes)
+    for box in boxes_copy:
+        box_height = box[1] + box[3]
+        height_lim = 0.9 * height
+        if box[1] == 0 or box[1] > height_lim or box_height == height:
+            contour_boxes.remove(box)
+    x, y, w, h = find_max_box(contour_boxes)
+    if h < 0.6 * height:
+        return (x0+x, y0+y, x0+x+w, y0+y+h)
+    else:
+        crop_img = thresh[y:y+h, x:x+w]
+        img = img[y:y+h, x:x+w]
+        height, width = crop_img.shape
+        kernel = np.ones((1, width), np.uint8)
+        dilation = cv2.dilate(crop_img, kernel, iterations=1)
+        locs = get_contour_boxes(dilation)
+        if len(locs) > 1:
+            x0, y0 = x0+x, y0+y
+            locs.sort(key=lambda t: t[2]*t[3], reverse=True)
+            locs = locs[:2]
+            locs.sort(key=lambda t: t[0])
+            x, y, w, h = locs[0]
+            first_line = (x0+x, y0+y, x0+x+w, y0+y+h)
+            x, y, w, h = locs[1]
+            second_line = (x0+x, y0+y, x0+x+w, y0+y+h)
+            return [first_line, second_line]
+        if len(locs) == 1:
+            return [(x0+x, y0+y, x0+x+w, y0+y+h)]
+
+
+def process_result(orig, ratio, result):
+    if type(result) is tuple:
+        x0, y0, x1, y1 = tuple(int(ratio * l) for l in result)
+        show_img(orig[y0:y1, x0:x1])
+
+    if type(result) is list and len(result) == 2:
+        x0, y0, x1, y1 = tuple(int(ratio * l) for l in result[0])
+        show_img(orig[y0:y1, x0:x1])
+        x0, y0, x1, y1 = tuple(int(ratio * l) for l in result[1])
+        show_img(orig[y0:y1, x0:x1])
+
+    if type(result) is list and len(result) == 1:
+        x0, y0, x1, y1 = tuple(int(ratio * l) for l in result[0])
+        show_img(orig[y0:y1, x0:x1])
+
+
+def detect_info(img):
+    img = cropout_unimportant_part(img)
+    orig = img.copy()
+    img, ratio = resize_img(img)
+    label_img = crop_label(img)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
+    threshold_img = get_threshold_img(label_img, kernel)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (label_img.shape[1], 5))
+    dilation = cv2.dilate(threshold_img, kernel, iterations=2)
+    contour_boxes = get_contour_boxes(dilation)
+    contour_boxes.sort(key=lambda t: t[2] * t[3], reverse=True)
+    contour_boxes = contour_boxes[:5]
+    info_list = get_info_list(img, contour_boxes)
+    # get number part
+    x, y, _, _ = info_list[0]
+    number_box = (0, 0, img.shape[1], info_list[0][1])
+    number_box = get_main_text(img, number_box, 5)
+    number_img = get_img_from_box(orig, ratio, number_box)
+    show_img(number_img)
+    # get name part
+    name_box = info_list[0]
+    name_box = get_name(img, get_main_text(img, name_box, 5))
+    name_img = get_img_from_box(orig, ratio, name_box)
+    show_img(name_img)
+    # get dob part
+    dob_box = info_list[1]
+    dob_box = get_main_text(img, dob_box, 5)
+    dob_img = get_img_from_box(orig, ratio, dob_box)
+    show_img(dob_img)
+    # get gender_and national part
+    gender_and_nationality_box = info_list[2]
+    gender_and_nationality_box = get_main_text(
+        img, gender_and_nationality_box, 5)
+    gender_n_nation_img = get_img_from_box(
+        orig, ratio, gender_and_nationality_box)
+    h, w, _ = gender_n_nation_img.shape
+    gender_img = gender_n_nation_img[0:h, 0:int(w/3)]
+    nation_img = gender_n_nation_img[0:h, int(w/3):int(0.85*w)]
+    show_img(gender_img)
+    show_img(nation_img)
+    # get country part
+    country_box = info_list[3]
+    result = get_text_from_two_lines(img, country_box)
+    process_result(orig, ratio, result)
+    address_box = info_list[4]
+    result = get_text_from_two_lines(img, address_box)
+    process_result(orig, ratio, result)
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import cv2
 import sys
 from cropper.cropper import crop_card
-from detector.detector import get_information
+from detector.detector import detect_info
 from PIL import Image
 import pytesseract
 import os
@@ -14,27 +14,22 @@ def show_img(img):
     cv2.imshow('', img)
     cv2.waitKey(0)
 
+warped = crop_card('test/{}.jpg'.format(3))
+detect_info(warped)
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-                help="Path to the image to be scanned")
-args = vars(ap.parse_args())
-warped = crop_card(args['image'])
-group_tuple = get_information(warped)
+# for index, t in enumerate(group_tuple):
+#     language = 'vie'
+#     if index == 0 or index == 2:
+#         language = 'eng'
 
-for index, t in enumerate(group_tuple):
-    language = 'vie'
-    if index == 0 or index == 2:
-        language = 'eng'
-
-    filename = "temp.png"
-    cv2.imwrite(filename, t)
-    text = pytesseract.image_to_string(Image.open(
-        filename), lang=language, config='--psm 7')
-    if index == 2:
-        numbers = re.findall(r'\d+', text)
-        text = '/'.join(numbers)
-    if index == 3:
-        text = text.split()[-1]
-    os.remove(filename)
-    print(text)
+#     filename = "{}.png".format(index)
+#     cv2.imwrite(filename, t)
+#     text = pytesseract.image_to_string(Image.open(
+#         filename), lang=language, config='--psm 7')
+#     if index == 2:
+#         numbers = re.findall(r'\d+', text)
+#         text = '/'.join(numbers)
+#     if index == 3:
+#         text = text.split()[-1]
+#     # os.remove(filename)
+#     print(text)
